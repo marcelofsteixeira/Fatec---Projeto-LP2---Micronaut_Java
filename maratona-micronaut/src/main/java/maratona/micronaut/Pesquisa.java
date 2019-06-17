900117b14a42545af07d297321c0ac400c550c83
marcelofsteixeira <48607320+marcelofsteixeira@users.noreply.github.com>
2019-06-17 16:02:48 -0300

    Update Pesquisa.java

--- maratona-micronaut/src/main/java/maratona/micronaut/Pesquisa.java
+++ maratona-micronaut/src/main/java/maratona/micronaut/Pesquisa.java
@@ -1,24 +1,41 @@
 package maratona.micronaut;
 
-public class Pesquisa {
-	private String tipo;
-	private String valor;
-	public Pesquisa(String tipo, String valor) {
-		super();
-		this.tipo = tipo;
-		this.valor = valor;
-	}
-	public String getTipo() {
-		return tipo;
-	}
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-	public String getValor() {
-		return valor;
-	}
-	public void setValor(String valor) {
-		this.valor = valor;
-	}
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
+public class Pesquisa {
+	
+	public static List<Log> pesquisarLogs(Map<String, String> pesq){
+		
+		ArrayList<String> campoPesq=new ArrayList<String>(pesq.keySet());
+		List<Log> resultado = new LinkedList<Log>();
+		List<Log> salvos = Application.getLogs();
+    	for(String campo:campoPesq) {
+    		if (campo.equals("status")) {
+    			for (Log log: salvos) {
+    				if (log.getStatus().equals(pesq.get("status"))) resultado.add(log);
+    			}
+    			return resultado;
+    		}
+    		
+    		if (campo.equals("id"))  {
+        			for (Log log: salvos) {
+        				if (log.getProblem().equals(pesq.get("id"))) resultado.add(log);
+        			}
+        			return resultado;
+        		}
+    		if (campo.equals("date"))  {
+    			for (Log log: salvos) {
+    				if (log.getDatetime().equals(pesq.get("date"))) resultado.add(log);
+    			}
+    			return resultado;
+    		}
+    	}
+    	return resultado;
+	}
+	
+	
+	
 }
