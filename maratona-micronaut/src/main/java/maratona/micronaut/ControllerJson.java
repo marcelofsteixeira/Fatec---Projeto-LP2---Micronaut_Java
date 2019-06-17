4631c0655f550e892d41e909e60a0623022fcc1e
marcelofsteixeira <48607320+marcelofsteixeira@users.noreply.github.com>
2019-06-16 21:39:59 -0300

    Update ControllerJson.java

--- maratona-micronaut/src/main/java/maratona/micronaut/ControllerJson.java
+++ maratona-micronaut/src/main/java/maratona/micronaut/ControllerJson.java
@@ -18,6 +18,9 @@ public class ControllerJson {
 	        return Single.just("Chamado Get");
 	    }
 	    
+	    //Chamado que recebe um Json com os mesmos atributos de um objeto Entrada no caminho "/application/Json/maratona"
+	    //O metodo do chamado cria um objeto Entrada com os mesmos atributos do Json, e executa o codigo python e as verificacoes em um objeto ProcPython.
+	    //O chamado retorna um objeto Saida com o status da verificacao do codigo, que é convertido para Json pelo framework.
 	    @Post("/maratona")
 	     public Single<Saida> verifCod(@Size (max=2048) @Body Entrada entro ) throws IOException, InterruptedException {
 	    	ProcPython pro = new ProcPython(entro);
