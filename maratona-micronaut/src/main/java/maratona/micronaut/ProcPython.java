b1a91ee5723c43212012aac42aefa0a5c4a9f4c0
marcelofsteixeira <48607320+marcelofsteixeira@users.noreply.github.com>
2019-06-16 21:33:35 -0300

    Update ProcPython.java

--- maratona-micronaut/src/main/java/maratona/micronaut/ProcPython.java
+++ maratona-micronaut/src/main/java/maratona/micronaut/ProcPython.java
@@ -20,6 +20,26 @@ public class ProcPython {
 		this.entrada = entrada;
 	}
 	
+	// 1 - E criada uma pasta com o nome "Run+Problema+Assinatura de tempo(milisegundos)", onde sera armazenado o arquivo .py (com a string convertida da base 64)
+	//Dependendo do tipo de problema, sao executados testes e gerados arquivos de saida, e a seguir os arquivos de saida sao verificados
+	//Um objeto saida e retornado pelo processo. Se os arquivos de resultados forem verificados como corretos, o atributo status do objeto sera "SUCCESS", senao sera "FAIL"
+	public Saida validarCod() throws IOException, InterruptedException {
+		String timeSig = Long.toString(System.currentTimeMillis());
+		String folderPath= System.getProperty("user.dir")+File.separator+"Run"+entrada.getProblem()+timeSig;
+		String pyPath=null;
+		if(new File(folderPath).mkdirs()) pyPath = folderPath+File.separator+"run.py";;
+		String source = new String(Base64.getDecoder().decode(entrada.getSourcecode()));
+		Writer sourceWriter= new PrintWriter(pyPath);
+		sourceWriter.write(source);
+		sourceWriter.close();
+		String resul = null;
+		if (entrada.getProblem().equals("A")) resul=ProcA(pyPath, folderPath);
+		if (entrada.getProblem().equals("B")) resul=ProcB(pyPath, folderPath);
+		return new Saida(entrada.getFilename(), entrada.getProblem(), resul);
+	}
+
+	
+	//2- Processamento de cada problema, incluindo geracao e verificacao de arquivos de resultados. Retorna a String de resultados ("SUCCESS" ou "FAIL")
 	public String ProcA(String pyPath, String folderPath) throws IOException, InterruptedException {
 		String result;
 		runPy(pyPath, folderPath, "SA1.txt", "A1.txt");
@@ -44,13 +64,14 @@ public class ProcPython {
 		return result;		
 	};
 	
-	
+	//3- Criacao de um processo para a execucaçao do codigo python que estara contido num objeto Pyexe. O metodo Pyrun desse objeto executa o codigo com o input inserido como uma String.
 	public void runPy(String pyPath, String folderPath, String arqSaida, String inputFile) throws IOException, InterruptedException {
 		String input = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+File.separator+"InputsPython"+File.separator+inputFile)), StandardCharsets.UTF_8);
 		Pyexe pyexe=new Pyexe(pyPath+" >"+folderPath+File.separator+arqSaida);
 		pyexe.Pyrun(input);
 	}
 	
+	//4- Verificacao de igualdade entre arquivos de resultados e arquivos esperados.
 	public boolean verifResult(String folderPath, String criado, String esperado) throws IOException {
 		Vector<String> resultCriado = new Vector <String>();
 		Vector<String> resultEsperado = new Vector<String>();
@@ -80,20 +101,5 @@ public class ProcPython {
 	
 	}
 	
-	
-	public Saida validarCod() throws IOException, InterruptedException {
-		String timeSig = Long.toString(System.currentTimeMillis());
-		String folderPath= System.getProperty("user.dir")+File.separator+"Run"+entrada.getProblem()+timeSig;
-		String pyPath=null;
-		if(new File(folderPath).mkdirs()) pyPath = folderPath+File.separator+"run.py";;
-		String source = new String(Base64.getDecoder().decode(entrada.getSourcecode()));
-		Writer sourceWriter= new PrintWriter(pyPath);
-		sourceWriter.write(source);
-		sourceWriter.close();
-		String resul = null;
-		if (entrada.getProblem().equals("A")) resul=ProcA(pyPath, folderPath);
-		if (entrada.getProblem().equals("B")) resul=ProcB(pyPath, folderPath);
-		return new Saida(entrada.getFilename(), entrada.getProblem(), resul);
-	}
 	
 }
