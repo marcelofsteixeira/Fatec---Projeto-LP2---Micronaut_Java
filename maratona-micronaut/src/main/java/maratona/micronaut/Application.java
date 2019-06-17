23c49cb9e9f589821bb130e9a92749f81b2bf75b
marcelofsteixeira <48607320+marcelofsteixeira@users.noreply.github.com>
2019-06-17 16:03:53 -0300

    Update Application.java

--- maratona-micronaut/src/main/java/maratona/micronaut/Application.java
+++ maratona-micronaut/src/main/java/maratona/micronaut/Application.java
@@ -6,7 +6,11 @@ import java.util.List;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-private List<Log> logs = new LinkedList<Log>();
+private static List<Log> logs = new LinkedList<Log>();
+	
+	public static List<Log> getLogs() {
+		return logs;
+	}
 	
     public static void main(String[] args) {
         Micronaut.run(Application.class);
