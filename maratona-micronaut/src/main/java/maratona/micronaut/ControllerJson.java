3523ad4f96f23cca7cd3d94821cf826faad1a9c9
marcelofsteixeira <48607320+marcelofsteixeira@users.noreply.github.com>
2019-06-17 16:54:57 -0300

    Update ControllerJson.java

--- maratona-micronaut/src/main/java/maratona/micronaut/ControllerJson.java
+++ maratona-micronaut/src/main/java/maratona/micronaut/ControllerJson.java
@@ -12,12 +12,64 @@ import io.micronaut.http.annotation.Body;
 import io.reactivex.Single;
 import io.micronaut.http.annotation.Controller;
 
+/* Este controller aceita os seguintes chamados REST:
+	
+	get - application/Json/maratona?pesquisa=valor
+	
+	Retorna uma lista de Jsons de uma pesquisa entre todos os Logs de execucao de cada codigo Python enviado. O campo pesquisa deve ser status, id ou date, com o valor sendo:
+	id: A ou B
+	status: SUCCESS ou FAIL
+	date: data no formato dd-mm-aa:hh:mm
+	
+	Somente um valor pode ser pesquisado por vez, e se mais de um valor é pesquisado, o chamado retorna uma lista com apenas o primeiro valor pesquisado.
+	
+	Exemplo de retorno:
+	
+	[
+    	{
+        "filename": "mergulho.py",
+        "problem": "B",
+        "sourcecode": "TiwgUiA9IFtpbnQoeCkgZm9yIHggaW4gaW5wdXQoKS5zcGxpdCgpXQplbnZpYWRvcyA9IGlucHV0KCkuc3BsaXQoKQppZiBOPT1SOgogICAgcHJpbnQoIioiKQplbHNlOgogICAgblJldG9ybmFkb3M9W10KICAgIGZvciBjIGluIHJhbmdlICgxLE4rMSk6CiAgICAgICAgaWYgc3RyKGMpIG5vdCBpbiBlbnZpYWRvczoKICAgICAgICAgICAgblJldG9ybmFkb3MuYXBwZW5kKHN0cihjKSkKICAgIHN0clJldG9yPSIgIi5qb2luKG5SZXRvcm5hZG9zKSsiICIKICAgIHByaW50KHN0clJldG9yKQ==",
+        "datetime": "17-06-19:16:38",
+        "status": "SUCCESS"
+   	 }
+	]
+	
+	
+	
+	post - application/Json/maratona
+	
+	Chamado que envia um json contendo um codigo fonte Python em base64, o tipo de problema (A ou B) e o nome do arquivo .py a ser criado. 
+	
+	Exemplo de Json de entrada:
+	
+	{
+	"filename": "mergulho.py",
+	"problem":"B",
+	"sourcecode":"TiwgUiA9IFtpbnQoeCkgZm9yIHggaW4gaW5wdXQoKS5zcGxpdCgpXQplbnZpYWRvcyA9IGlucHV0KCkuc3BsaXQoKQppZiBOPT1SOgogICAgcHJpbnQoIioiKQplbHNlOgogICAgblJldG9ybmFkb3M9W10KICAgIGZvciBjIGluIHJhbmdlICgxLE4rMSk6CiAgICAgICAgaWYgc3RyKGMpIG5vdCBpbiBlbnZpYWRvczoKICAgICAgICAgICAgblJldG9ybmFkb3MuYXBwZW5kKHN0cihjKSkKICAgIHN0clJldG9yPSIgIi5qb2luKG5SZXRvcm5hZG9zKSsiICIKICAgIHByaW50KHN0clJldG9yKQ=="
+	}
+	
+	O sistema roda o codigo fonte e valida os resultados. Se forem os esperados retorna uma mensagem de sucesso ou falha. 
+	
+	Exemplo de Json de saida:
 
+
+	{
+   	"filename": "mergulho.py",
+    	"problem": "B",
+   	"status": "SUCCESS"
+	}
+
+
+
+*/
 
 
 @Controller("/application/Json") 
 public class ControllerJson {
 	
+	    //Chamado que recebe parametros de pesquisa que realizam uma busca pela lista de objetos Log contidos na classe Application (os logs de envio de cada codigo Python) processado no sistema
+	    //Os valores pesquisados são recebidos como um Map<String,String> e o processamento da pesquisa é realizado pela classe Pesquisa
 	    @Get("/maratona{?pesq*}")
 	    public List<Log> pesq (Map<String, String> pesq) {
 	    	return Pesquisa.pesquisarLogs(pesq);
