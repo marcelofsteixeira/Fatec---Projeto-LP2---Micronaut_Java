cc1ec17ddc9ab4f9dc9792e7502b75e19f3a4a92
marcelofsteixeira <48607320+marcelofsteixeira@users.noreply.github.com>
2019-06-17 16:59:20 -0300

    Update Application.java

--- maratona-micronaut/src/main/java/maratona/micronaut/Application.java
+++ maratona-micronaut/src/main/java/maratona/micronaut/Application.java
@@ -6,6 +6,7 @@ import java.util.List;
 import io.micronaut.runtime.Micronaut;
 
 //Em sistemas windows, a aplicacao é iniciada executando o arquivo gradlew, localizado no diretorio do projeto. Por padrao, o servidor roda na porta 8181. 
+//Antes da execucao do codigo, e necessaria a configuracao de uma variavel de ambiente PYTHON_PATH, que aponta para o executavel do interpretador Python do sistema
 //A classe Application possui uma lista de objetos Log, que reunem informacoes sobre cada execucao de codigo python
 public class Application {
 private static List<Log> logs = new LinkedList<Log>();
